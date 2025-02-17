d91d8824b10d09bdeeafd66e60fda54b508880f3
chaima limayem <chaimalimayem1@gmail.com>
2025-02-17 15:45:06 +0100

    creation du serveur

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const  helmet  = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(cors())
+app.use(helmet())
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello from the server"});
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening...`);
+}); 
+
